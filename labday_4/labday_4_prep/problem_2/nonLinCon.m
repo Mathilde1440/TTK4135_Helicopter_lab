0464446b085106018a157bee109254deac73f597
Mathilde <mathilde.skaset.haar@gmail.com>
2026-04-06 13:51:57 +0200

    remove unnecessary files

--- labday_4/labday_4_prep/problem_2/nonLinCon.m
+++
@@ -1,9 +0,0 @@
-function [c,ceq] = nonLinCon(z,alpha, beta,lambda_t,N,mx)
-
-c = zeros(N,1);
-
-for i = 1:N
-c(i) = alpha*exp(-beta*(z(1 + (i-1)*mx) - lambda_t)^2) - z(5+ (i-1)*mx);
-end
-ceq = [];
-end
